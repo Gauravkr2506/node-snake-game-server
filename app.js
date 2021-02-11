97188a0aa5729b73bb4b8191ffcb70b16431fed3
gauravkr2506 <gauravkr2506@gmail.com>
2021-02-11 20:32:12 +0530

    deploy

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3003;
+const port = process.env.PORT || 8080;
 const http = require("http").createServer();
 
 const io = require("socket.io")(http);
